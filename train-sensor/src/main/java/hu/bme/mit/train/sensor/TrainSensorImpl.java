56195bfb0579c79b66aae82bbef3034aaa897796
LaszloAbrok <laszlopatrikabrok@gmail.com>
2023-04-17 19:34:19 +0200

    all task are done

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -10,6 +10,7 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainUser user;
 	private int speedLimit = 5;
 
+
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
 		this.user = user;
@@ -24,6 +25,11 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+		if(this.speedLimit<0 || this.speedLimit>500) user.setAlarmState(true);
+		int diff = controller.getReferenceSpeed() - this.speedLimit;
+		if(diff >  controller.getReferenceSpeed()/2){
+			user.setAlarmState(true);
+		}
 	}
 
 }
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,15 +5,52 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 public class TrainSensorTest {
+
+    TrainController controller;
+    TrainUser user;
+    TrainSensor sensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller, user);
+    }
+
+    
+    @Test
+    public void AlertSpeedLimitIsAbove500() {    
+        sensor.overrideSpeedLimit(501);
+        when(user.getAlarmState()).thenReturn(true);
+    }
+
+    
+    @Test
+    public void AlertSpeedLimitIsBelow0() {
+        sensor.overrideSpeedLimit(200);
+        when(user.getAlarmState()).thenReturn(false);
     }
 
     @Test
+    public void DifferencetIsLargerThan50Percent() {
+        sensor.overrideSpeedLimit((int)0.2*controller.getReferenceSpeed());
+        when(user.getAlarmState()).thenReturn(true);
+    }
+
+    @Test
+    public void DifferenceLimitIsSmallerThan50Percent() {
+        sensor.overrideSpeedLimit((int)0.8*controller.getReferenceSpeed());
+        when(user.getAlarmState()).thenReturn(false);
+    }
+
+    /* 
+    @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
-    }
+    }*/
 }
